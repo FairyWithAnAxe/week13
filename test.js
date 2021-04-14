aaa37934ce6035e43995019f0b26725b90641fcd
FairyWithAnAxe <71983766+FairyWithAnAxe@users.noreply.github.com>
2021-04-15 00:45:18 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,48 @@
+/*
+let name = "Ilya";
+alert (`hello ${1}`); //hello 1
+alert (`hello ${"name"}`); //hello name
+alert (`hello ${name}`); //hello Ilya
+*/
+
+/*
+var x=5;
+var y=2;
+var z=x+y;
+alert (z);
+*/
+
+/*
+function showX(x)
+{ 
+	return 
+    x;
+}
+alert(showX(28));
+*/
+
+/*
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+//alert(a + b); // возвращает 12
+alert (+a + +b); // возвращает 3
+*/
+
+/*
+x = 5; 
+x++;
+x -= 2;
+alert(x);
+*/
+
+/*
+function sum(x, y=0) {
+    return x+y;
+} 
+alert(sum(5));
+*/
+
+//alert(true + false);
+//alert(6 / "3");
+//alert(" \t \n" - 2);
